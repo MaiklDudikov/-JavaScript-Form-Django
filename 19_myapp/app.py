acde47c63d71e925e8f7bfbff3fd4da6bd602f35
MaiklDudikov <wmiwa87@gmail.com>
2024-07-30 20:02:36 +0200

    add 'register.html' и 'login.html'

--- 19_myapp/app.py
+++ 19_myapp/app.py
@@ -2,6 +2,8 @@ import sqlite3
 
 
 # Подключаемся к базе данных или создаем ее
-connection = sqlite3.connect('database.db')
+# connection = sqlite3.connect('database.db')
 # Создаем курсор
-cursor = connection.cursor()
+# cursor = connection.cursor()
+
+# print(sqlite3.sqlite_version)
