2f1901a9311b9da7dea3a3234243c31f2cf2e963
MaiklDudikov <wmiwa87@gmail.com>
2024-09-11 19:07:49 +0200

    add lesson_22

---
+++ 22_lesson_11.09/main_22.js
@@ -0,0 +1,10 @@
+let student = {
+    name: "Alan",
+    surname: "German",
+    age: 18,
+    group: "A1",
+    faculty: "Programming"
+    };
+    console.log(student.name);
+    console.log(student.surname);
+    console.log(student.age);
