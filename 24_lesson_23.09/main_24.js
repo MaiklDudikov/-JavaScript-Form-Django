3bc5ac1dc89e5cd04b851df422803284c0cc0e5e
MaiklDudikov <wmiwa87@gmail.com>
2024-09-23 20:03:25 +0200

    add lesson_24.3

--- 24_lesson_23.09/main_24.js
+++ 24_lesson_23.09/main_24.js
@@ -1,17 +1,91 @@
-let request;
+let subbliBtn = document.getElementById("submit-btn");
+subbliBtn.onclick = function(){
+    let nameValue = document.getElementById("name-inp").value;
+    let phonValue = document.getElementById("phone-inp").value;
+    let data = "name=" + encodeURIComponent(nameValue) + "&phone=" + encodeURIComponent(phonValue);
+    let request;
 
-if(window.XMLHttpRequest){
-    request = new XMLHttpRequest();
-}
-else{
-    request = new ActiveXObject("Microsoft.XMLHTTP");
-}
-
-request.open("GET", "person.json");
-request.onload = function(){
-    if(request.status === 200){
-        alert(request.response);
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
     }
+    else {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    request.open("POST", "server.php");
+
+    request.onreadystatechange = function(){
+        if (request.readyState == 4 && request.status == 200) {
+            alert("Здравствуйте " + nameValue + "! Мы перезвоним вам через 1 минуту" );
+            }
+        }
+
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.send(data);
 }
 
-request.send();
+
+// let request;
+
+// if(window.XMLHttpRequest){
+//     request = new XMLHttpRequest();
+// }
+// else{
+//     request = new ActiveXObject("Microsoft.XMLHTTP");
+// }
+
+// request.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=Kiev&units=metric&APPID=b03a2cfad336d11bd9140ffd92074504");
+
+// request.onload = function(){
+//     if(request.status === 200){
+//         console.log(request.response);
+//     }
+// }
+
+// request.send();
+
+
+// let request;
+
+// if(window.XMLHttpRequest){
+//     request = new XMLHttpRequest();
+// }
+// else{
+//     request = new ActiveXObject("Microsoft.XMLHTTP");
+// }
+
+// request.open("GET", "tex.txt");
+// request.onloadend = function(event){
+//     alert('Загужено ${event.loaded}');
+// }
+
+// request.onerror = function(){
+//     alert("Ошибка");
+// }
+
+// request.onload = function(){
+//     if(request.status === 404){
+//         alert(request.response);
+//     }
+// }
+
+// request.send();
+
+
+// let request;
+
+// if(window.XMLHttpRequest){
+//     request = new XMLHttpRequest();
+// }
+// else{
+//     request = new ActiveXObject("Microsoft.XMLHTTP");
+// }
+
+// request.open("GET", "person.json");
+// request.onload = function(){
+//     if(request.status === 200){
+//         alert(request.response);
+//     }
+// }
+
+// request.send();
